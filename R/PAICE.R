6eff673b906758799ad8cc8081f3c7adf3d3d44a
Alberto J. Coello <47033657+albertojcoello@users.noreply.github.com>
2022-09-13 15:53:42 +0200

    Add files via upload

---
+++ R/PAICE.R
@@ -0,0 +1,11 @@
+#' PAICE: Phylogeographic Analysis of Colonization Events
+#'
+#' PAICE package has three main functions:
+#' colonizations, rarecol and maxCol
+#'
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name PAICE
+NULL
